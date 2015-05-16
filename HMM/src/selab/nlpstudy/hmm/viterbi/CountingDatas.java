840f32d1a32d6b1483245abc594ae0ed03cf390a
yourhrh <asd236678@gmail.com>
2015-05-16 20:25:25 +0900

    Refactoring & readCountBigram

--- HMM/src/selab/nlpstudy/hmm/viterbi/CountingDatas.java
+++ HMM/src/selab/nlpstudy/hmm/viterbi/CountingDatas.java
@@ -7,6 +7,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.function.Function;
 
 import selab.nlpstudy.hmm.training.TrainingData;
 
@@ -18,36 +19,39 @@ public class CountingDatas {
 		// TODO Auto-generated method stub
 		morphemeCount = new HashMap<String, Integer>();
 		bigramCount = new HashMap<ArrayList<String>, Integer>();
-		try {
-			
-			BufferedReader morphemeReader = new BufferedReader(new FileReader(new File("./MorphemeCount.txt")));
+		
+		readDatatoHashMap("./MorphemeCount.txt", (s) -> {
+			String[] morphemeData = s.split("\\s+");
+			morphemeCount.put(morphemeData[0],Integer.parseInt(morphemeData[1]));
+			return null;
+		});
+		
+		readDatatoHashMap("./BigramCount.txt", (s) -> {
+			String changed = s.replaceAll("\\[","").replaceAll(",", "").replaceAll("\\]", "");
+			ArrayList<String> bigramList = new ArrayList<String>(); 
+			String[] bigramData = changed.split("\\s+");
+			System.out.println(bigramData[0] + " " + bigramData[1] + " " +  bigramData[2]);
+			bigramList.add(bigramData[0]); bigramList.add(bigramData[1]);
+			bigramCount.put(bigramList, Integer.parseInt(bigramData[2]));
+			return null;
+		});
+		
+		
+	}
+	
+	private void readDatatoHashMap(String fileName,Function<String, Void> makeHashMap){
+		try{
+			BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
 			String s;
-			while((s = morphemeReader.readLine())!=null){
-				String[] morphemeData = s.split("\\s+");
-				morphemeCount.put(morphemeData[0],Integer.parseInt(morphemeData[1]));
-			}
-			
-			BufferedReader bigramReader = new BufferedReader(new FileReader(new File("./BigramCount.txt")));
-			while((s=bigramReader.readLine())!=null){
-				s.replace("\\[",""); s.replace(",", ""); s.replace("\\]", "");
-				ArrayList<String> bigramList = new ArrayList<String>(); 
-				String[] bigramData = s.split("\\s+");
-				bigramList.add(bigramData[0]); bigramList.add(bigramData[1]);
-				bigramCount.put(bigramList, Integer.parseInt(bigramData[2]));
-			}
-			
-			
-			
-		} catch (FileNotFoundException e) {
+			while((s = reader.readLine()) != null)
+				makeHashMap.apply(s);
+		}catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
 	}
 
 	public HashMap<String, Integer> getMorphemeCount() {
@@ -57,7 +61,7 @@ public class CountingDatas {
 
 	public HashMap<ArrayList<String>, Integer> getBigramCount() {
 		// TODO Auto-generated method stub
-		return null;
+		return bigramCount;
 	}
 
 	public HashMap<TrainingData, Integer> getStateCount() {
